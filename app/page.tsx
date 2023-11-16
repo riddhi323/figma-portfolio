ca7a50904a9dc840f8ab9c98382bf39d11f1ab7a
= <mukherjee4004@gmail.com>
2023-11-16 14:31:55 +0530

    image gallery add update #1

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,50 @@
-import Hero from '@/components/main/Hero'
-import Myimage from '@/components/main/Myimage'
-import Skills from '@/components/main/Skills'
-import Image from 'next/image'
-
+"use client";
+import Hero from "@/components/main/Hero";
+import Myimage from "@/components/main/Myimage";
+import Skills from "@/components/main/Skills";
+import Image from "next/image";
+import Typewriter from "typewriter-effect";
 export default function Home() {
   return (
-   <main className = "h-full w-full">
-    <div className = "flex flex-col gap-20 ">
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20 ">
+        <Hero />
+        {/* <Skills/> */}
 
-      <Hero/>
-      {/* <Skills/> */}
+        <Myimage />
 
-      <Myimage/>
+        
 
-      <div className = "w-full h-[500px] text-white">hello</div>
-    </div>
-    
-   </main>
-  )
+        <div className="  moving-div  md:hidden w-full relative flex flex-col h-[500px] text-white text-center">
+          <div className="   p-2 max-w-4xl absolute top-[-10px] rounded-lg left-[70px] w-[230px] mx-auto  h-[400px]  ">
+            <span className = "text-emerald-500 text-3xl font-mono">This is</span>
+            <Typewriter
+              options={{
+                strings: ["Bibhabendu Mukherjee"],
+                autoStart: true,
+                cursorClassName: "Typewriter__wrapper",
+                loop: true,
+                
+                delay: 40,
+                deleteSpeed: 20,
+              }}
+            />
+            <span className = "text-purple-500 text-3xl">I'm a </span>
+            <Typewriter
+              
+              options={{
+                strings: ["Computer Science Student" , "Recreational Programmer" , "Love to make System"],
+                autoStart: true,
+                cursorClassName: "Typewriter__wrapper",
+                loop: true,
+                
+                delay: 80,
+                deleteSpeed: 50,
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
--- components/main/Myimage.tsx
+++ components/main/Myimage.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react'
 
 function Myimage() {
@@ -6,28 +7,46 @@ function Myimage() {
         return Math.floor(Math.random() * 360); // Generate a random rotation between 0 and 360 degrees
       };
   return (
-    <div className = "image-slider mt-[30px]   ">
+    <div className="flex flex-row flex-wrap">
+      <div className = "image-slider mt-[30px] md:ml-[400px] z-[1]  ">
 
-        <span style = {{transform : `rotateY(calc(1*45deg)) translateZ(350px)`}} >
-            <img src="/5.png"  alt = "myimage"/>
-        </span>
+<span style = {{transform : `rotateY(calc(1*45deg)) translateZ(350px)`}} >
+    <img src="/5.png" className = "animate-pulse"  alt = "myimage"/>
+</span>
 
-        <span style = {{transform : `rotateY(calc(2*45deg)) translateZ(350px)`}} >
-            <img src="/5.png" className="w-[60px] h-[60px]" alt = "myimage"/>
-        </span>
+<span style = {{transform : `rotateY(calc(2*45deg)) translateZ(350px)`}} >
+    <Image src="/5.png" width= {100}   className = "animate-pulse"   height= {0}alt = "myimage"/>
+</span>
 
-        <span style = {{transform : `rotateY(calc(3*45deg)) translateZ(350px)`}} >
-            <img src="/5.png" alt = "myimage"/>
-        </span>
-        <span style = {{transform : `rotateY(calc(4*45deg)) translateZ(350px)`}} >
-            <img src="/5.png" alt = "myimage"/>
-        </span>
-        <span style = {{transform : `rotateY(calc(5*45deg)) translateZ(350px)`}} >
-            <img src="/5.png" alt = "myimage"/>
-        </span>
+<span style = {{transform : `rotateY(calc(3*45deg)) translateZ(350px)`}} >
+<Image src="/5.png" width= {100}  className = "animate-pulse"    height= {0}alt = "myimage"/>
+</span>
+<span style = {{transform : `rotateY(calc(4*45deg)) translateZ(350px)`}} >
+    <img src="/5.png"  className = "animate-pulse"   alt = "myimage"/>
+</span>
+<span style = {{transform : `rotateY(calc(5*45deg)) translateZ(350px)`}} >
+    <img src="/5.png"  className = "animate-pulse"   alt = "myimage"/>
+</span>
+
+</div>
+
+
+    <div className= "relative ">
+        <div className=" hidden lg:block xl:left-[500px] absolute lg:left-[300px] top-[100px]">
+        <div className = " h-[50px] w-[300px] ">
+        <h1 className="text-4xl leading-9 font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
+    myself Bibhabendu Mukherjee
+</h1>
+
+        </div>
+        </div>
+        
+    </div>
 
     </div>
+  
   )
 }
 
 export default Myimage
+
